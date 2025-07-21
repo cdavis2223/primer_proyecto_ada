e85286f7358a86a14f39fabea6a1c3aea19c9280
cdavis2223 <cdavis2223@gmail.com>
2025-07-21 15:45:41 -0600

    segundo commit mejoras en el while

--- cinthya_davis.py
+++ cinthya_davis.py
@@ -7,25 +7,32 @@ letras_adivinadas = []
 letras_incorrectas = []
 
 while vidas > 0:
+    print("\n" + "-"*30)
     mostrar = ""
     for letra in palabra:
-        if letra in letras_adivinadas:
-            mostrar += letra + " "
-        else:
-            mostrar += "_ "
-            
+        mostrar += letra + " " if letra in letras_adivinadas else "_ "
     print("Palabra:", mostrar.strip())
     print("Vidas:", vidas)
-    print("Letras incorrectas:", letras_incorrectas)
+    print("Letras incorrectas:", ", ".join(letras_incorrectas))
+
+    if all(letra in letras_adivinadas for letra in palabra):
+        print("\n ¡Ganaste! La palabra era:", palabra)
+        break
 
     intento = input("Ingresa una letra: ").lower()
     if len(intento) != 1 or not intento.isalpha():
         print("Por favor, ingresa solo una letra.")
         continue
 
+    if intento in letras_adivinadas or intento in letras_incorrectas:
+        print("Ya ingresaste esa letra. Intenta con otra.")
+        continue
+
     if intento in palabra:
         letras_adivinadas.append(intento)
     else:
-        if intento not in letras_incorrectas:
-            letras_incorrectas.append(intento)
-            vidas -= 1
+        letras_incorrectas.append(intento)
+        vidas -= 1
+
+if vidas == 0:
+    print("\n ¡Perdiste! La palabra era:", palabra)
